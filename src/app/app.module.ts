55d2b3fdd4d0b58a87fd47caac634c8a8846c860
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-08-03 08:30:21 -0500

    install flex layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
@@ -24,6 +24,7 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     BrowserAnimationsModule,
     CoreModule,
+    FlexLayoutModule,
     AppRoutingModule
   ],
   providers: [],
